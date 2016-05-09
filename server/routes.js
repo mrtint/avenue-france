f8261e4837692eb20485a8bf274bb3c7bb64e3b0
Mr.Tint <mrtint0729@gmail.com>
2016-05-09 10:45:16 +0900

    thing 은 단순한 예제니까 라우팅에서도 제외

--- server/routes.js
+++ server/routes.js
@@ -8,7 +8,7 @@ var path = require('path');
 
 module.exports = function (app) {
     // Insert routes below
-    app.use('/api/things', require('./api/thing'));
+    // app.use('/api/things', require('./api/thing'));
     app.use('/api/users', require('./api/user'));
     app.use('/api/videos', require('./api/video'));
     app.use('/api/comments', require('./api/comment'));
