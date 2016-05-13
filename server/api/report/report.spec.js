4f67d75de9741fccbf892ed9ea367e02eab029ed
Mr.Tint <mrtint0729@gmail.com>
2016-05-13 11:19:14 +0900

    list 의 조회 방법에 대한 테스트 코드 추가 thing 디렉토리는 더이상 필요하지 않아서 삭제

--- server/api/report/report.spec.js
+++ server/api/report/report.spec.js
@@ -4,11 +4,11 @@ var should = require('should');
 var app = require('../../app');
 var request = require('supertest');
 
-describe('GET /api/things', function() {
+describe('GET /api/reports', function() {
 
   it('should respond with JSON array', function(done) {
     request(app)
-      .get('/api/videos')
+      .get('/api/reports')
       .expect(200)
       .expect('Content-Type', /json/)
       .end(function(err, res) {
